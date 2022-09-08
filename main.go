a2ca08b763b0011d3e05834ed997db8adfc21f09
weichuanjun <weichuanjun0528@gmail.com>
2022-09-08 18:38:43 +0900

    practise go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	var test2 int = 2
+	var (
+		a = 1
+		b = 2
+	)
+
+	fmt.Print(a, b)
+	fmt.Printf("zhi is %v", test2)
+	fmt.Println(a,b)     
+	for i := range test2 {
+		if i != 3; i++ {
+		print("hello") 
+			println()
+			 var m = 64;
+			 pri
+			 print dsrfdsfdsconst
+			printLn("hello,")
+			
+
+
+
+
+		
+
+
+
+		weihcuajdjfsjdfjd
+		}
+
+	
+	}
+}
